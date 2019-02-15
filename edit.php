b417f421c841933dd34227b9ac0ac36823c03872
Tommy Wang <tommywang@Tommys-MacBook-Pro.local>
2019-02-15 13:23:49 -0500

    changed numerous input fields into textarea fields

--- edit.php
+++ edit.php
@@ -171,6 +171,11 @@ li {
     resize:vertical;
 
 }
+
+#top-div textarea{
+  border-color: #D6D7DB;
+  border-width: 1.5px;
+}
 #bottom-div {
     float: left;
     width: 100%;
@@ -298,35 +303,35 @@ li {
       <form name="readme" action="edit-dd.php?selection=<?php echo $selection;?>&id=<?php echo $id;?>" method="POST" >
         <input type="hidden" name="id" value="<?php echo $id; ?>"/>
         <input type="hidden" name="sde_old" value="<?php echo $sde_name; ?>">
-        <li>Common Name:</li><li><input type="text" name="common_name" style="width:500px;" value="<?php echo $common_name; ?>"/></li>
+        <li>Common Name</li><li><input type="text" name="common_name" style="width:500px;" value="<?php echo $common_name; ?>"/></li>
         <br>
-        <li>SDE Name:</li><li><input type="text" name="sde_name" style="width:500px;" value="<?php echo $sde_name; ?>" required/></li>  <!-- this field is made mandatory by adding 'required' -->
+        <li>SDE Name</li><li><input type="text" name="sde_name" style="width:500px;" value="<?php echo $sde_name; ?>" required/></li>  <!-- this field is made mandatory by adding 'required' -->
 
         <br>
-        <li>Tags for Guide:</li><li><input type="text" name="tags_guide" style="width:500px;" value="<?php echo $tags_guide; ?>"/></li>
+        <li>Tags for Guide</li><li><input type="text" name="tags_guide" style="width:500px;" value="<?php echo $tags_guide; ?>"/></li>
         <br>
-        <li>Tags for SDE:</li><li><input type="text" name="tags_sde" style="width:500px;" value="<?php echo $tags_sde; ?>"/></li>
+        <li>Tags for SDE</li><li><input type="text" name="tags_sde" style="width:500px;" value="<?php echo $tags_sde; ?>"/></li>
         <br>
-        <li>Summary:</li><li><textarea name="summary" style="height:90px;width:500px;" ><?php echo $summary; ?> </textarea></li>
+        <li>Summary</li><li><textarea name="summary" style="height:90px;width:500px;" ><?php echo $summary; ?> </textarea></li>
         <br>
-        <li>Summary - Update Date:</li><li><input type="text" name="summary_update_date" style="height:40px;width:500px;" value="<?php echo $summary_update_date; ?>"/></li>
+        <li>Summary - Update Date</li><li><input type="text" name="summary_update_date" style="height:40px;width:500px;" value="<?php echo $summary_update_date; ?>"/></li>
         <br>
-        <li>Description:</li><li><textarea name="description" style="height:90px;width:500px;"> <?php echo $description; ?> </textarea></li>
+        <li>Description</li><li><textarea name="description" style="height:90px;width:500px;"> <?php echo $description; ?> </textarea></li>
         <br>
-        <li>Description - Data Location:</li><li><input type="text" name="description_data_loc" style="width:500px;" value="<?php echo $description_data_loc; ?>"/></li>
+        <li>Description - Data Location</li><li><input type="text" name="description_data_loc" style="width:500px;" value="<?php echo $description_data_loc; ?>"/></li>
         <br>
-        <li>Data Steward:</li><li><input type="text" name="data_steward" style="width:500px;" value="<?php echo $data_steward; ?>"/></li>
+        <li>Data Steward</li><li><input type="text" name="data_steward" style="width:500px;" value="<?php echo $data_steward; ?>"/></li>
         <br>
-        <li>Data Engineer:</li><li><input type="text" name="data_engineer" style="width:500px;" value="<?php echo $data_engineer; ?>"/></li>
+        <li>Data Engineer</li><li><input type="text" name="data_engineer" style="width:500px;" value="<?php echo $data_engineer; ?>"/></li>
         <br>
-        <li>Credits:</li><li><input type="text" name="credits" style="width:500px;" value="<?php echo $credits; ?>"/></li>
+        <li>Credits</li><li><input type="text" name="credits" style="width:500px;" value="<?php echo $credits; ?>"/></li>
         <br>
-        <li>General Constraints Use Limitations:</li><li><input type="text" name="genconst" style="width:500px;"value="<?php echo $genconst; ?>"/></li>
+        <li>General Constraints Use Limitations</li><li><textarea name="genconst" style="height:90px;width:500px;" ><?php echo $genconst; ?></textarea></li>
         <br>
-        <li>Legal Constraints Use Limitations:</li><li><input type="text" name="legconst" style="width:500px;" value="<?php echo $legconst; ?>"/></li>
+        <li>Legal Constraints Use Limitations</li><li><textarea name="legconst" style="height:90px;width:500px;" ><?php echo $legconst; ?></textarea></li>
         <br>
         <!-- <li>Update Frequency:</li><li><input type="text" name="update_freq" /></li> -->
-        <li>Update Frequency:</li>
+        <li>Update Frequency</li>
         <select required name="update_freq" id="ddTables" style="font-size:12pt; height:45px; width:500px;">
           <option value="<?php echo $update_freq; ?>" selected><?php echo $update_freq; ?></option>
           <?php
@@ -334,25 +339,25 @@ li {
           ?>
         </select>
         <br>
-        <br><li>Date of Last Update:</li><li><input type="text" name="date_last_update" style="width:500px;" value="<?php echo $date_last_update; ?>"/></li>
+        <br><li>Date of Last Update</li><li><input type="text" name="date_last_update" style="width:500px;" value="<?php echo $date_last_update; ?>"/></li>
         <br>
-        <li>Date of Underlying Data:</li><li><input type="text" name="date_underlying_data" style="width:500px;" value="<?php echo $date_underlying_data; ?>"/></li>
+        <li>Date of Underlying Data</li><li><input type="text" name="date_underlying_data" style="width:500px;" value="<?php echo $date_underlying_data; ?>"/></li>
         <br>
-        <li>Data Source:</li><li><input type="text" name="data_source" style="width:500px;" value="<?php echo $data_source; ?>"/></li>
+        <li>Data Sources and Compilation Process</li><li><textarea name="data_source" style="height:90px;width:500px;" ><?php echo $data_source; ?></textarea></li>
         <br>
-        <li>Version:</li><li><input type="text" name="version" style="width:500px;" value="<?php echo $version; ?>"/></li>
+        <li>Version</li><li><input type="text" name="version" style="width:500px;" value="<?php echo $version; ?>"/></li>
         <br>
-        <li>Common Uses:</li><li><input type="text" name="common_uses" style="width:500px;" value="<?php echo $common_uses; ?>"/></li>
+        <li>Common Uses</li><li><textarea name="common_uses" style="height:90px;width:500px;" ><?php echo $common_uses; ?></textarea></li>
         <br>
-        <li>Data Quality:</li><li><input type="text" name="data_quality" style="width:500px;" value="<?php echo $data_quality; ?>"/></li>
+        <li>Data Quality</li><li><textarea name="data_quality" style="height:90px;width:500px;"><?php echo $data_quality; ?></textarea></li>
         <br>
-        <li>Caveats:</li><li><input type="text" name="caveats" style="width:500px;" value="<?php echo $caveats; ?>"/></li>
+        <li>Caveats</li><li><textarea name="caveats" style="height:90px;width:500px;"><?php echo $caveats; ?></textarea></li>
         <br>
-        <li>Future Plans:</li><li><input type="text" name="future_plans" style="width:500px;" value="<?php echo $future_plans; ?>"/></li>
+        <li>Future Plans</li><li><textarea name="future_plans" style="height:90px;width:500px;"><?php echo $future_plans; ?></textarea></li>
         <br>
-        <li>Distribution:</li><li><input type="text" name="distribution" style="width:500px;" value="<?php echo $distribution; ?>"/></li>
+        <li>Distribution</li><li><input type="text" name="distribution" style="width:500px;" value="<?php echo $distribution; ?>"/></li>
         <br>
-        <li>Contact:</li><li><input type="text" name="contact" style="width:500px;" value="<?php echo $contact; ?>"/></li>
+        <li>Contact</li><li><input type="text" name="contact" style="width:500px;" value="<?php echo $contact; ?>"/></li>
         <br>
         <br>
         <input type="submit" id="tableSubmit" value="Save" name="readme_save_button"/>
