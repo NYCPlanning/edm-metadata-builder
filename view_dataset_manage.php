4b01d241e998da3eb93fd09eddadfda8d077482d
tommyswang <tommyswang@gmail.com>
2019-04-08 20:01:27 -0400

    Added add collaborators functionality

--- view_dataset_manage.php
+++ view_dataset_manage.php
@@ -7,23 +7,26 @@ $sde = $_GET['sde'];
 $common = $_GET['common'];
 $delete_email = $_GET['delete_email'];
 
+if(isset($_GET['add_email'])) {
+  $email = $_GET['add_email'];
+  $a_query ="INSERT INTO collaboration(sdename, email) VALUES ('$sde','$email')";
+  pg_query($a_query);
+}
+
 if(isset($_GET['delete'])) {
-  if($email == $delete_email && $u_type == 'basic'){
+  if($email == $delete_email){
     $d_query = "DELETE FROM collaboration WHERE sdename = '$sde' AND email = '$delete_email'";
     pg_query($d_query);
     echo '<script>';
     echo 'window.location.href="view_dataset.php"';  //not showing an alert box.
     echo '</script>';
-  } elseif($u_type == 'basic') {
+  } else {
     $d_query = "DELETE FROM collaboration WHERE sdename = '$sde' AND email = '$delete_email'";
     pg_query($d_query);
-  } elseif ($u_type == 'admin' || $u_type == 'superuser'){
-      echo '<script>';
-      echo 'alert("User cannot be removed.")';  //not showing an alert box.
-      echo '</script>';
   }
 
 }
+
 $query = "SELECT email FROM collaboration WHERE sdename = '$sde' ORDER BY email";
 $result = pg_query($query);
 $row =pg_fetch_all($result);
@@ -31,11 +34,37 @@ $row =pg_fetch_all($result);
 
 
 ?>
+<style>
+  .collab_header {
+    display:inline-block;
+  }
 
+  .collab-add-button {
+    border:none;
+  }
+
+  .collab-add-button i {
+    color: black;
+    font-size: 20px;
+  }
+
+  .collab-add-button i:hover, .fa-trash-alt:hover {
+    color: #D96B27;
+  }
+
+  .fa-trash-alt {
+    color: black;
+  }
+
+  #email {
+    width: 70%;
+  }
+
+
+</style>
 <!-- Modal -->
-<div id="exportReadme" class="modal fade " role="dialog" style="margin-top: 200px;">
+<div id="addCollab" class="modal fade " role="dialog" style="margin-top: 200px;">
   <div class="modal-dialog">
-
     <!-- Modal content-->
     <div class="modal-content ">
       <div class="modal-header">
@@ -44,34 +73,33 @@ $row =pg_fetch_all($result);
       </div>
       <div class="modal-body">
         <div class="">
-          <form class="" method="post">
-
-            <div class="col-md-8">
-              <label for="email">Email:</label>
-              <input type="text" name="email" id="email">
-            </div>
-            <input type="submit" name="submit" value="Add">
-          </form>
-
+          <div class="text-center">
+            <form method="get" action="view_dataset_manage.php">
+                <label for="email">Email:</label>
+                <input type="text" name="add_email" id="email">
+                <input type="hidden" name="sde" value="<?php echo $sde?>">
+                <input type="hidden" name="common" value="<?php echo $common?>">
+                <input type="submit" name="submit" value="Add">
+            </form>
+          </div>
         </div>
-
       </div>
-
     </div>
-
   </div>
 </div>
 
 <div class="container">
   <h3><?php echo $common; ?></h3>
   <hr>
-  <h3>Collaborators</h3>
-  <button type="button" class="btn btn-default btn-rounded mb-4 export-file" data-toggle="modal" data-target="#exportReadme"><i class="fas fa-plus-circle"></i></button>
+  <h3 class="collab_header">Collaborators</h3>
+  <button type="button" data-toggle="modal" data-target="#addCollab" class="collab-add-button">
+    <i class="fas fa-plus-circle"></i>
+  </button>
   <br>
   <?php
     // Displays all the retrieved data from the database
     foreach($row as $r) {
-      echo "<p style='display:inline-block;'>".$r["email"]."  </p><a id=".$r["email"].">  <i id=".$r["email"]." class='far fa-trash-alt' onClick='deleteUser(this.id)'></i></a><br>";
+      echo "<p><a id=".$r["email"].">  <i id=".$r["email"]." class='far fa-trash-alt' onClick='deleteUser(this.id)' style='margin-right:5px;'></i></a>".$r["email"]."  </p>";
 
     }
   ?>
@@ -79,14 +107,10 @@ $row =pg_fetch_all($result);
 <script>
 
    function deleteUser(id) {
-       if (confirm("Are you sure you want to delete this user?"))
-       {
+       if (confirm("Are you sure you want to delete this user?")) {
            window.location.href= "view_dataset_manage.php?delete=True&sde=<?php echo $sde.'&common='.$common.'&delete_email=';?>"+id;
        }
-       else
-       {
-          window.location.href = "view_dataset_manage.php?sde=<?php echo $sde.'&common='.$common.'';?>";
-       }
+
    }
 
 </script>
